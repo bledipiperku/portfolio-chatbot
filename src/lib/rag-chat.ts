72b6e77cf3a2a61c21e117d61708c15849a76d1b
Bledi Piperku <bledip2003@gmail.com>
2025-08-13 17:06:07 +0200

    fix: chatbot model

--- src/lib/rag-chat.ts
+++ src/lib/rag-chat.ts
@@ -1,7 +1,9 @@
-import { RAGChat, upstash } from "@upstash/rag-chat";
+import { RAGChat, groq } from "@upstash/rag-chat";
 import { redis } from "./redis";
 
 export const ragChat = new RAGChat({
-  model: upstash("meta-llama/Meta-Llama-3-8B-Instruct"),
+  model: groq("llama3-8b-8192", {
+    apiKey: process.env.GROQ_API_KEY,
+  }),
   redis: redis,
 });

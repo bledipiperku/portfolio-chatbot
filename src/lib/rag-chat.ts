5e67a40329824fce90b4252ce861f5a71f91fda8
Bledi Piperku <bledip2003@gmail.com>
2026-02-25 16:47:47 +0100

    fix: llama 3.1

--- src/lib/rag-chat.ts
+++ src/lib/rag-chat.ts
@@ -2,7 +2,7 @@ import { RAGChat, groq } from "@upstash/rag-chat";
 import { redis } from "./redis";
 
 export const ragChat = new RAGChat({
-  model: groq("llama3-8b-8192", {
+  model: groq("llama-3.1-8b-instant", {
     apiKey: process.env.GROQ_API_KEY,
   }),
   redis: redis,
